cb9da3a12373e4fe30821e29e1b02d00012f1e53
R. Martinho Fernandes <rmf@rmf.io>
2023-11-29 15:55:05 +0100

    Cleanup archive script

--- archive.sh
+++ archive.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-cd dist; zip -r -FS ../../clarkesreader.zip * --exclude *.map; cd ..; zip -r -FS ../clarkesreader.src.zip index.ts icons/ resources/ manifest.json package.json README yarn.lock
+cd dist
+zip -r -FS ../../clarkesreader.zip * --exclude *.map
+cd ..
+zip -r -FS ../clarkesreader.src.zip index.ts manifest.json package.json yarn.lock README icons/ resources/ 
